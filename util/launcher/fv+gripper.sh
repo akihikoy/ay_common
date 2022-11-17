6c9573ebf7608d7ec340505ed92625cb5a21434b
Akihiko Yamaguchi <info@akihikoy.net>
2022-11-17 11:36:49 +0900

    Added launcher scripts for demo;

---
+++ util/launcher/fv+gripper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#\file    fv+ur-demo.sh
+#\brief   FV+UR-demo launcher script.
+#\author  Akihiko Yamaguchi, info@akihikoy.net
+#\version 0.1
+#\date    Oct.03, 2022
+
+#ROS Configuration
+#ROS_DISTR=kinetic
+ROS_DISTR=melodic
+. /opt/ros/$ROS_DISTR/setup.bash
+. ~/catkin_ws/devel/setup.bash
+. /opt/ros/$ROS_DISTR/share/rosbash/rosbash
+
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_IP=127.0.0.1
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:$HOME/ros_ws:${HOME}/prg/ay_test/ros
+
+rosrun fv_gripper_ctrl ctrl_panel.py -dxl_dev=USB0
