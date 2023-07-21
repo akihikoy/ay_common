de52ebf7e7c4055f371ca1ad02dace7e54090548
Akihiko Yamaguchi <info@akihikoy.net>
2023-07-21 13:06:59 +0900

    Improved the utility scripts;

--- util/setup/fv+gripper-setup.sh
+++ util/setup/fv+gripper-setup.sh
@@ -5,8 +5,23 @@
 #\version 0.1
 #\date    Jul.21, 2023
 
+answer_yes='n'
+function print_usage
+{
+  echo "Usage: $0 [-y]"
+  echo "  -y Answer yes to all queries."
+}
+while getopts 'y' flag; do
+  case "${flag}" in
+    y) answer_yes='y' ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
 function ask
 {
+  if [ "${answer_yes}" == "y" ]; then return 0; fi
   while true; do
     echo -n '  (y|n) > '
     read s
